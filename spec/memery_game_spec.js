24998c68c417ecb20e59dfe41803ebbf1bbb3eba
Ndiyakholwa <mnqanqenindiyakholwa206@gmail.com>
2024-09-18 14:46:17 +0200

    formatting

--- spec/memery_game_spec.js
+++ spec/memery_game_spec.js
@@ -3,40 +3,39 @@ const { JSDOM } = require("jsdom");
 const fs = require("fs");
 const path = require("path");
 const mockWindow = {
-    location: {
-      reload: jasmine.createSpy(),
-    },
-  };
-
+  location: {
+    reload: jasmine.createSpy(),
+  },
+};
 
 describe("script", function () {
   let dom;
   let document;
   let window;
 
-  const html = fs.readFileSync(path.resolve(__dirname, "../index.html"), "utf-8");
+  const html = fs.readFileSync(
+    path.resolve(__dirname, "../index.html"),
+    "utf-8"
+  );
 
   dom = new JSDOM(html, {
-    runScripts: "dangerously"
+    runScripts: "dangerously",
   });
   document = dom.window.document;
   window = dom.window;
   global.window = window;
   global.document = document;
-  const { populate,restart } = require("../src/index");
+  const { populate, restart } = require("../src/index");
 
   restart();
   populate();
 
   it("should create the correct number of game cards", function () {
-
-    
     const cards = document.querySelectorAll(".item");
     expect(cards.length).toBe(16);
   });
 
   it("should add 'cardOpen' class when a card is clicked", function () {
-   
     const firstCard = document.querySelectorAll(".item")[0];
     firstCard.click();
 
@@ -47,44 +46,43 @@ describe("script", function () {
     let matchedCards = document.querySelectorAll(".cardMatch");
 
     for (let i = 0; i < matchedCards.length; i++) {
-        matchedCards[i].classList.remove("cardMatch");
-      }
+      matchedCards[i].classList.remove("cardMatch");
+    }
 
     const cards = document.querySelectorAll(".item");
-    
+
     cards[0].innerHTML = "🧳";
     cards[1].innerHTML = "🧳";
-    
+
     cards[0].click();
     cards[1].click();
-    
-   
+
     matchedCards = document.querySelectorAll(".cardMatch");
-    expect(matchedCards.length).toBe(2); 
+    expect(matchedCards.length).toBe(2);
   });
 
   it("should trigger confetti when all cards are matched", async function () {
     const cards = document.querySelectorAll(".item");
-    window.confetti = jasmine.createSpy('confetti');
-  
-    for (let i = 0; i < cards.length; i+=2) {
+    window.confetti = jasmine.createSpy("confetti");
+
+    for (let i = 0; i < cards.length; i += 2) {
       cards[i].innerHTML = "🧳";
-      cards[i+1].innerHTML = "🧳";
+      cards[i + 1].innerHTML = "🧳";
       cards[i].click();
-      cards[i+1].click();
-      await new Promise(resolve => setTimeout(resolve, 700));
+      cards[i + 1].click();
+      await new Promise((resolve) => setTimeout(resolve, 700));
     }
-  
-    await new Promise(resolve => setTimeout(resolve, 700));
-  
+
+    await new Promise((resolve) => setTimeout(resolve, 700));
+
     const matchedCards = document.querySelectorAll(".cardMatch");
-    expect(matchedCards.length).toBe(cards.length); 
+    expect(matchedCards.length).toBe(cards.length);
     expect(window.confetti).toHaveBeenCalled();
   });
-  
+
   it("should prevent flipping matched cards again", async function () {
     const cards = document.querySelectorAll(".item");
-    
+
     cards[0].innerHTML = "🧳";
     cards[1].innerHTML = "🧳";
     cards[0].click();
@@ -92,21 +90,20 @@ describe("script", function () {
 
     expect(cards[0].classList.contains("cardMatch")).toBe(true);
     expect(cards[1].classList.contains("cardMatch")).toBe(true);
-    
+
     cards[0].click();
     cards[1].click();
-    
-    await new Promise(resolve => setTimeout(resolve, 700));
- 
+
+    await new Promise((resolve) => setTimeout(resolve, 700));
+
     expect(cards[0].classList.contains("cardOpen")).toBe(false);
     expect(cards[1].classList.contains("cardOpen")).toBe(false);
   });
-  
 
   it("should reset the game when the restart button is clicked", function () {
     global.window = mockWindow;
     const resetButton = document.querySelector(".reset");
-  
+
     resetButton.click();
     expect(mockWindow.location.reload).toHaveBeenCalled();
 
@@ -114,5 +111,4 @@ describe("script", function () {
     window = dom.window;
     global.window = window;
   });
-  
 });
